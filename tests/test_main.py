9a6b75dace1bebc19794a4ffd2860697af3b7142
davidlesicnik <david@lesicnik.eu>
2025-04-28 19:11:08 +0200

    changed website text

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,4 +5,4 @@ def test_hello():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b'Hello, world!' in response.data
+    assert b'Hello, David!' in response.data
